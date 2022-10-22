798b819403eac0f7d7d0d0e5d53843434594510f
mohamed-elrefai <mohamed.mostafa0699@gmail.com>
2022-10-22 15:09:06 +0200

    Update: System

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config'
-import express, {Application} from 'express';
+import express, {Application, Response} from 'express';
 import MongoDB from './Database/Mongo.DB';
 import setupOther from './Utils/Others/setup.other';
 import indexHooks from './Hooks/index.hooks';
@@ -14,3 +14,36 @@ setupOther(app)
 
 // Routers
 indexHooks(app)
+
+app.get('/', (res: Response) => {
+    res.status(200).json({
+        status: "OK",
+        Number: 200,
+        message: "Success",
+        message2: "To UnderStand API GO",
+        API: {
+            Auth:{
+                Register: "/register",
+                Login: "/login"
+            },
+            User: {
+                Get_Profile: "/user/",
+                UpdateProfile: "/user/settings/update",
+            },
+            Halls: {
+                Create_Halls: "/create/halls",
+                Get_Single_Page: "/get/:username/hall/:id",
+                Get_All_Halls: "/get/all/:types",
+                Update_Hall: "/hall/settings/update/:id",
+                Delete_Hall: "/hall/settings/delete/:id"
+            },
+            Cars: {
+                Create_Cars: "/create/cars",
+                Get_Single_Page: "/get/:username/cars/:id",
+                Get_All_Cars: "/get/all/:types",
+                Update_Car: "/car/settings/update/:id",
+                Delete_Car: "/car/settings/delete/:id"
+            }
+        }
+    })
+})
